784e170da5015908295797eb414ad66a6ce47eba
FelipeLoureiroQA <30761959+FelipeLoureiroQA@users.noreply.github.com>
2024-04-04 21:56:31 +0000

    feat: add request and response helpers

---
+++ handler/request.go
@@ -0,0 +1,45 @@
+package handler
+
+//CrateOpening
+
+import "fmt"
+
+func errParamIsRequired(name, t string) error {
+	return fmt.Errorf("error: param %s is required and should be a %s", name, t)
+}
+
+type CreateOpeningRequest struct {
+	Role     string `json:"role"`
+	Company  string `json:"company"`
+	Location string `json:"location"`
+	Remote   *bool  `json:"remote"`
+	Link     string `json:"link"`
+	Salary   int64  `json:"salary"`
+}
+
+func (r *CreateOpeningRequest) Validate() error {
+	//Tratamento para nenhum parametro poder faltar
+	if r.Role == "" && r.Company == "" && r.Location == "" && r.Remote == nil && r.Salary <= 0 {
+		return fmt.Errorf("malformed request body")
+	}
+	if r.Role == "" {
+		return errParamIsRequired("role", "string")
+	}
+	if r.Company == "" {
+		return errParamIsRequired("company", "string")
+	}
+	if r.Location == "" {
+		return errParamIsRequired("location", "string")
+	}
+	if r.Remote == nil {
+		return errParamIsRequired("remote", "bool")
+	}
+	if r.Link == "" {
+		return errParamIsRequired("link", "string")
+	}
+	if r.Salary == 0 {
+		return errParamIsRequired("salary", "int64")
+	}
+
+	return nil
+}
